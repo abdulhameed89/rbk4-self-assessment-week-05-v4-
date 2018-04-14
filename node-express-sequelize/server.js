028ddfa2eaaa2c46cd50ba8e6e768d23d17b1001
abdulhameed89 <abd.cs.rbk@gmail.com>
2018-04-14 09:57:21 +0300

    assesment 5

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -6,12 +6,17 @@ var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage
 var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
+//creating users route to get all users and send it to the '/users' route 
+app.get('/users',function (req,res) {
+	res.send(Model.findAll({attributes:['username']}))
+})
 
-/*  Create a '/users' route that responds to 
-    a GET request with all users in the database */
-
-
-
+ var port=3000;
+//set the enviroment port if it is not exist it will take the local port 3000
+app.set('port', process.env.PORT || 3000);
+// listening to any change on this server 
+app.listen(port)
+// exports the methods that we could use it somewhare else
 module.exports = { 
   app: app,
   User: User
